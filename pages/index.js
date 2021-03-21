9afac1ea453b0f202ace137f87a1f1ce0759844d
ercumentlacin <forever.ducale@gmail.com>
2021-03-21 23:55:27 +0300

    Dynamic Route

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import unfetch from 'isomorphic-unfetch'
+import { v4 as uuidv4 } from 'uuid'
+import Link from 'next/link'
+import string_to_slug from '../helpers/titleToSlug'
 
-function Home() {
+function Home({ items }) {
+  // console.log(items)
+
   return (
     <Layout>
       <Head>
@@ -10,8 +16,32 @@ function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <h1 className="bg-primary">Hello, Next.js!</h1>
+      <ul>
+        {items?.map(({ title }) => {
+          const url = string_to_slug(title)
+          return (
+            <li key={uuidv4()}>
+              <Link href="/post/[id]" as={`/post/${url}`}>
+                <a>{title}</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const data = await unfetch(
+    'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ercumentlacin'
+  )
+  const result = await data.json()
+  return {
+    props: {
+      items: result.items
+    }
+  }
 }
 
 export default Home
---
+++ pages/post/[id].js
@@ -0,0 +1,55 @@
+import unfetch from 'isomorphic-unfetch'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
+import string_to_slug from '../../helpers/titleToSlug'
+
+const Post = ({ items }) => {
+  const router = useRouter()
+  const { pid } = router.query
+  console.log(items)
+  return (
+    <Layout>
+      {items.map(({ title, categories }) => (
+        <article>
+          <header>
+            <h2>{title}</h2>
+            <p>
+              <strong>Kategoriler: </strong>
+              {categories?.join(', ')}
+            </p>
+          </header>
+        </article>
+      ))}
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const data = await unfetch(
+    'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ercumentlacin'
+  )
+  const result = await data.json()
+  return {
+    paths: result?.items?.map(({ title }) => {
+      return { params: { id: string_to_slug(title) } }
+    }),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const data = await unfetch(
+    'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ercumentlacin'
+  )
+  const result = await data.json()
+  return {
+    props: {
+      items: result.items.filter(
+        (item) => string_to_slug(item.title) === params.id
+      )
+    }
+  }
+}
+
+export default Post
